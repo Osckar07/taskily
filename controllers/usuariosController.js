04473d4c75c3b107b31570b8ebf84532fa90ed40
Osckar07 <50966035+Osckar07@users.noreply.github.com>
2020-08-11 15:51:01 -0600

    modificando usuarioscontroller segun lo solicitado

--- controllers/usuariosController.js
+++ controllers/usuariosController.js
@@ -1,6 +1,26 @@
 // Importar el modelo
 const Usuario = require("../models/Usuario");
 
+const passwordValidator = require('password-validator');
+
+var schema = new passwordValidator();
+schema
+// minimo 8 caracteres
+.is().min(8)                                  
+// maximo 10 caracteres
+.is().max(100)                                
+// por lo menos una mayuscula
+.has().uppercase()                            
+// por lo menos una minuscula
+.has().lowercase()                             
+// por lo menos un numero
+.has().digits(1)                               
+//  no debe contener espacios
+.has().not().spaces()                           
+// contraseñas restringidas
+.is().not().oneOf(['Contrasena123', 'Password123']); 
+
+
 exports.formularioCrearCuenta = (req, res, next) => {
   res.render("registrarse", { layout: "auth" });
 };
@@ -8,21 +28,33 @@ exports.formularioCrearCuenta = (req, res, next) => {
 exports.crearCuenta = async (req, res, next) => {
   // Obtener los datos de la nueva cuenta
   // Obtener por destructuring
-  const { fullname, email, password } = req.body;
+  const { fullname, email, password, cpassword } = req.body;
 
-  // Intentar crear el usuario
-  try {
-    // Crear el usuario
-    await Usuario.create({
-      fullname,
-      email,
-      password,
-    });
+  if (req.body.password == req.body.cpassword) {
+    if(schema.validate(password)){
+      // Intentar crear el usuario
+    try {
 
-    // Redireccionar el usuario al formulario de inicio de sesión
-    res.redirect("iniciar_sesion");
-  } catch (error) {
-    res.render("registrarse", { layout: "auth", error: error.message });
+      // Crear el usuario
+      await Usuario.create({
+        fullname,
+        email,
+        password,
+      });
+
+      // Redireccionar el usuario al formulario de inicio de sesión
+      res.redirect("iniciar_sesion");
+    } catch (error) {
+      res.render("registrarse", { layout: "auth", error: error.message });
+    }
+    }
+    else{
+      res.render("registrarse", { layout: "auth", error: "La contraseña debe contener una mayuscula, una minuscula, un numero, debe ser mayor a 8 caracteres y menor que 100 y no debe contener espacios" });
+    }
+    
+  }
+  else{
+    res.render("registrarse", { layout: "auth", error: "Las contraseñas no coinciden" });
   }
 };
 
